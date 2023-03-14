0b351037963eccb5f2f0b2439d2da46e9c97cba9
Linn Strand <linnber@msn.com>
2023-03-14 15:23:22 +0100

    frontend added

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,16 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import Spinner from '../components/Spinner';
+
+function Dashboard() {
+  return (
+    <>
+      <section className="heading">
+        <p> Dashboard</p>
+      </section>
+    </>
+  );
+}
+
+export default Dashboard;
